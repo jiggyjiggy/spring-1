2291e3c96453c6bae3a574c00c41826fccaa538e
jiggy <jiggyjiggy0323@gmail.com>
2022-06-28 14:22:07 +0900

    컴포넌트 스캔과 자동 의존관계 설정

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -3,15 +3,19 @@ package hello.hellospring.service;
 import hello.hellospring.domain.Member;
 import hello.hellospring.repository.MemberRepositoy;
 import hello.hellospring.repository.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class MemberService {
 
 
     private final MemberRepositoy memberRepositoy;
 
+    @Autowired
     public MemberService(MemberRepositoy memberRepositoy) {
         this.memberRepositoy = memberRepositoy;
     }
